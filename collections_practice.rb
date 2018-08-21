d6046a9f09559f67e0d5385dbce72009f4a3845f
Jonathan Chan <johnkchan252@gmail.com>
2018-08-21 23:54:56 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,8 +1,8 @@
-# def sort_array_asc(array)
-#   array.sort do |a, b|
-#     a <=> b
-#   end
-# end
+def sort_array_asc(array)
+  array.sort do |a, b|
+    a <=> b
+  end
+end
 
 # def sort_array_desc(array)
 #   array.sort do |a, b|
